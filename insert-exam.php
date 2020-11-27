7f9845c97c9dc298f915afa7339921ddf19f7634
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-11-27 12:46:58 +0700

    Update insert-exam.php

--- insert-exam.php
+++ insert-exam.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'itf-exam.mysql.database.azure.com', 'itf@itf-exam' , '159Pook159', 'new-exam', 3306);
+mysqli_real_connect($conn, 'itf-exam.mysql.database.azure.com', 'itf@itf-exam' , '159Pook159', 'ift', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
